d694f772b8ed11046bbefc413ce3599ca84af47e
Nadir Ali <alinadir0509@gmail.com>
2020-04-24 19:45:36 +0530

    Updates

---
+++ precache-manifest.54a82677f78ba213d457f8c1aca5d768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a2156b2496fa57f8ce36c2c653be95",
+    "url": "/To-do-React/index.html"
+  },
+  {
+    "revision": "b5dd56340056f8348bc0",
+    "url": "/To-do-React/static/css/main.457bf392.chunk.css"
+  },
+  {
+    "revision": "8f40300df89a0f7ae257",
+    "url": "/To-do-React/static/js/2.0e54b6d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-React/static/js/2.0e54b6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5dd56340056f8348bc0",
+    "url": "/To-do-React/static/js/main.0566a584.chunk.js"
+  },
+  {
+    "revision": "8e3f18f0c806a53216a9",
+    "url": "/To-do-React/static/js/runtime-main.0ab8b475.js"
+  }
+]);
